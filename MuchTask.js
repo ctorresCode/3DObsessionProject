67e94ce4270f36e440f23b6f3f2f225dcbf37f25
ctorresCode <ctorrescode@gmail.com>
2024-08-02 11:54:37 -0500

    Creación de PDF con toda la información de los inputs

--- MuchTask.js
+++ MuchTask.js
@@ -4,8 +4,7 @@ function repetirDiv(){
     var newDiv = document.createElement('div');
     newDiv.className = 'cloned-div';
     newDiv.innerHTML = `
-    
-         <div class="bg-[#cbcbcb40] flex flex-col mt-5 p-4 h-[990px] md:p-[75px] md:w-[1000px] md:h-[990px] md:justify-center md:justify-center md:items-center" id="Task"> 
+    <div class="bg-[#cbcbcb40] flex flex-col mt-5 p-4 h-[990px] md:p-[75px] md:w-[1000px] md:h-[990px] md:justify-center md:justify-center md:items-center" id="Task"> 
             <p class="text-lg text-left mt-2 md:text-2xl">Number task<span class="text-red-600">*</span></p>
             <input id="numberTask" class="w-[313px] h-10 mt-3 md:h-[190px] md:w-[500px] md:p-2" type="number">
             <p class="text-lg text-left mt-6 md:text-2xl">Name task<span class="text-red-600">*</span></p>
@@ -17,8 +16,14 @@ function repetirDiv(){
             <p class="text-lg text-left mt-6 md:text-2xl" x-on:loadstart="text-left">Type task<span class="text-red-600">*</span></p>
             <select id="typeTask" class="w-[313px] h-10 mt-4 md:h-[120px] md:w-[500px] md:text-2xl md:p-2">
                 <option>Type</option>
-                <option>Canada</option>
-                <option>Mexico</option>
+                <option>Básica</option>
+                <option>Técnica</option>
+                <option>UX</option>
+                <option>Soporte/Administración</option>
+                <option>Seguridad</option>
+                <option>Integración</option>
+                <option>Regresión</option>
+                <option>Validación</option>
             </select>
             <p class="text-lg text-left mt-5 md:text-2xl">Estimated start date<span class="text-red-600">*</span></p>
             <input id="estimatedTask" class="w-[313px] h-10 mt-4 md:h-[190px] md:w-[500px] md:p-2 " type="date">
@@ -36,11 +41,8 @@ function repetirDiv(){
         <div class="mt-12">
             <button id="plusTask" class="text-4xl text-[#50B498]" onclick="repetirDiv()">+</button>
         </div>
-
-        <div id="clonecontainer">
         
-        </div>
-    </div>
+    
     `;
     var container = document.getElementById('clonecontainer');
     container.appendChild(newDiv);
